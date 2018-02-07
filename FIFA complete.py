d481cb902dcfdbe7c99e244e1b9d27d1ef8cbf87
rputra <rhpmail@gmail.com>
2018-02-07 22:18:15 +0700

    polynomial regression for overall vs value

--- FIFA complete.py
+++ FIFA complete.py
@@ -34,14 +34,45 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(regressor, X_test, y_test, scoring='r2')
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
 # Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Overall vs Value (Test set)')
+plt.title('Overall vs Value (Linear Regression)')
 plt.xlabel('Overall')
 plt.ylabel('Value')
 plt.show()
+
+# Predicting a new result with Linear Regression
+print(regressor.predict(94))
+
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(regressor, X_test, y_test, scoring='r2', cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Overall')
+plt.ylabel('Value')
+plt.show()
+
+# Predicting a new result with Polynomial Regression
+print(lin_reg_2.predict(poly_reg.fit_transform(94)))
+
+from sklearn.pipeline import Pipeline
+pipeline = Pipeline([("polynomial_features", poly_reg),("linear_regression", lin_reg_2)])
+pipeline.fit(X_poly, y)
+
+scores = cross_val_score(pipeline, X_test, y_test, scoring='r2', cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
